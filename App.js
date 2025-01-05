01bb2c9706e7b53292571dd148a59d03175ee072
PrashantMudhiraj <prashantchevula@gmail.com>
2025-01-05 20:04:33 +0530

    First commit

---
+++ App.js
@@ -0,0 +1,20 @@
+/**
+ * <div id="parent">
+ *     <div id="child">
+ *          <h1>H1 Tag</h1>
+ *          <h2>H2 Tag</h2>
+ *     </div>
+ * </div>
+ */
+
+const heading = React.createElement("h1", { id: "heading" }, "Hello React");
+const parent = React.createElement(
+    "div",
+    { id: "parent" },
+    React.createElement("div", { id: "child" }, [
+        React.createElement("h1", {}, "H1 Tag"),
+        React.createElement("h2", {}, "H2 Tag"),
+    ])
+);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
